6ffa05d96c6166fd947a543ac361c54d9202a530
Eric Tran <erictran0007@gmail.com>
2024-05-13 11:18:59 +1000

    Initiated project with MongoDB configured

---
+++ src/test/java/dev/eric/MovieGenie/MovieGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eric.MovieGenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
